70144a73150f980da0302d94a97619fd5850aad6
JangGeunSong <sjg1468@gmail.com>
2022-02-20 21:38:43 +0900

    테스트 코드 바탕 작성 [service 부분]

---
+++ src/test/java/com/myproject/todo/service/DailyTodoServiceTest.java
@@ -0,0 +1,14 @@
+package com.myproject.todo.service;
+
+import org.junit.jupiter.api.Test;
+
+public class DailyTodoServiceTest {
+
+    @Test
+    public void saveTodoTest() {
+
+        
+
+    }
+
+}
